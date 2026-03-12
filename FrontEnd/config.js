8764ad723b550b20458e4c352c9970baa293e0b5
shubhamshirgave9-ctrl <shubhamshirgave9@gmail.com>
2026-03-12 13:02:33 +0530

    Update API_BASE to Render URL to fix network errors online

--- FrontEnd/config.js
+++ FrontEnd/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    API_BASE: 'http://localhost:10000'
+    API_BASE: 'https://phisheye-backend.onrender.com'
 };
 console.log("PhishEye Config Loaded: ", CONFIG.API_BASE);
 window.CONFIG = CONFIG;
